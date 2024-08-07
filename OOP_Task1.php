3c8f2ceea53c1a669f0ccf1845cc27ba5f21ed21
MohamedOsamaGalal98 <146726249+MohamedOsamaGalal98@users.noreply.github.com>
2024-08-07 21:51:52 +0300

    adding files

---
+++ OOP_Task1.php
@@ -0,0 +1,110 @@
+<?php
+
+
+class BookStore {
+
+	 private $books = [
+				"Science"		=>	[ 	[ "Name"			=>	[ "Chimestory" ],
+										  "PublishingDate"	=>	[ "1900" ],
+										  "Author"			=>	[ "Marwan" ],
+										  "Availability"	=>	["Available"]
+										],
+									    [ "Name"			=>	[ "Space" ],
+										  "PublishingDate"	=>	[ "1850" ],
+										  "Author"			=>	[ "Alaa" ],
+										  "Availability"	=>	["Available"]
+										] 
+									],
+
+
+				
+				"History" 		=>	[   [ "Name"			=>	[ "Islamic History" ],
+										  "PublishingDate"	=>	[ "1980" ],
+										  "Author"			=>	[ "Osama" ],
+										  "Availability"	=>	["Available"]
+										],
+
+							 		    [ "Name"			=>	[ "Romanian History" ],
+										  "PublishingDate"	=>	[ "1860" ],
+										  "Author"			=>	[ "Mohamed" ],
+										  "Availability"	=>	["Available"]
+										]
+									],
+				
+
+
+				"Engineering"   =>	[   [ "Name"			=>	[ "Mechanical" ],
+										  "PublishingDate"	=>	[ "2020" ],
+										  "Author"			=>	[ "Ahmed" ],
+										  "Availability"	=>	["Available"]
+										],
+
+							 		    [ "Name"			=>	[ "Electrical" ],
+										  "PublishingDate"	=>	[ "1800" ],
+										  "Author"			=>	[ "Galal" ],
+										  "Availability"	=>	["Available"]
+										]
+									],
+				
+
+
+				"Fantasy"		=>	[	[ "Name"			=>	[ "The Hobbit" ],
+										  "PublishingDate"	=>	[ "2001" ],
+										  "Author"			=>	[ "Mohamed" ],
+										  "Availability"	=>	["Available"]
+										],
+
+										[ "Name"			=>	[ "Harry Potter" ],
+										  "PublishingDate"	=>	[ "1998" ],
+										  "Author"			=>	[ "Peter" ],
+										  "Availability"	=>	["Available"]
+										]
+									]
+							];
+
+ 	public $borrowedBooks;
+
+ 	public $customers;
+
+ 	public $availableBooks;
+
+
+	public function addBook($field, $name) {
+
+	    //$books[$field] = $name;
+	    array_push($books[$field], $name);
+	    echo 'The Book With Name ' . $name . ' Added To Field ' . $field . ' In The Book Store';
+	    echo "<pre>";
+
+		print_r($books);
+
+		echo "</pre>";	
+	}
+
+	public function removeBook($field, $name) {
+
+	}
+
+	public function borrowBook($field, $name) {
+
+	}
+
+	public function returnBackBook($field, $name) {
+
+	}
+
+}
+
+
+class Book extends BookStore {
+
+	public $Features = [
+						"Field"		=>	[ "Engineering" ],
+						"Name" 		=>	[ "Mechatronics" ],
+						"PublishingDate"   =>	[ "2016" ],
+						"Author"		=>	[ "Mohamed Osama" ]
+						];
+
+}
+
+
